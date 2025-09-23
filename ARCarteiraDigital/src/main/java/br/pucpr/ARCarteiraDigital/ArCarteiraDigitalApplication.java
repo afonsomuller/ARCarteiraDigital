7e11a3bb7da4aa5fc6f3794fdc0a7186a339a3be
Afonso <afonso.muller2@gmail.com>
2025-09-23 19:59:38 -0300

    Inicialização do projeto

---
+++ ARCarteiraDigital/src/main/java/br/pucpr/ARCarteiraDigital/ArCarteiraDigitalApplication.java
@@ -0,0 +1,13 @@
+package br.pucpr.ARCarteiraDigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArCarteiraDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArCarteiraDigitalApplication.class, args);
+	}
+
+}
---
+++ ARCarteiraDigital/src/test/java/br/pucpr/ARCarteiraDigital/ArCarteiraDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.pucpr.ARCarteiraDigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArCarteiraDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
